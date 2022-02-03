2bc055011f30b3c923033255c70cb28e8c847f53
Joy Chakraborty <chakrabortyjoy638@gmail.com>
2022-02-03 21:10:48 +0530

    Day 101 & 102 code upload

---
+++ Day102/Ifelse.java
@@ -0,0 +1,19 @@
+public class Ifelse{
+    public static void main(String args[]){
+        int i,m=0,flag=0;
+        int n=69;
+        m=n/2;
+        if(n==0||n==1){
+            System.out.println(n+" is not a prime number");
+        }else{
+            for(i=2;i<=m;i++){
+                if(n%i==0){
+                    System.out.println(n+" is not a prime number");
+                    flag=1;
+                    break;
+                }
+            }
+            if(flag==0)  { System.out.println(n+" is a prime number"); }
+        }
+    }
+}
