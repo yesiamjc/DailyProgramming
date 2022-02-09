9ab3ab89bda932564aa78d1d7088b5bd5a78fd17
Joy Chakraborty <chakrabortyjoy638@gmail.com>
2022-02-09 22:25:25 +0530

    Day 108 Code Upload

---
+++ Day108/array.java
@@ -0,0 +1,13 @@
+public class array {
+    public static void main(String args[]) {
+        int a[] = new int[5];
+        a[0] = 10;
+        a[1] = 20;
+        a[2] = 70;
+        a[3] = 40;
+        a[4] = 50;
+        for (int i = 0; i < a.length; i++)
+            System.out.println(a[i]);
+
+    }
+}
