4a091ac0a8d1ea4c286a88ed931d0aa56a937ee5
Joy Chakraborty <chakrabortyjoy638@gmail.com>
2022-02-17 21:56:22 +0530

    Day 116 Code Upload

---
+++ Day116/main.java
@@ -0,0 +1,13 @@
+public class main {
+    public static void main(String[] args) {
+      int time = 22;
+      if (time < 10) {
+        System.out.println("Good morning.");
+      } else if (time < 20) {
+        System.out.println("Good day.");
+      }  else {
+        System.out.println("Good evening.");
+      }
+    }
+  }
+  
