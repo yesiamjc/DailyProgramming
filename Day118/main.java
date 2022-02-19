18856925aff845b450dc54fa9487d382cdee081c
Joy Chakraborty <chakrabortyjoy638@gmail.com>
2022-02-19 19:18:54 +0530

    Day 118 Code Upload

---
+++ Day118/main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine(); 
+        System.out.print("You have entered: " + str);
+    }
+}
