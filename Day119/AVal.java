136a9fd218c8d6f9a041761c7a687496554a661b
Joy Chakraborty <chakrabortyjoy638@gmail.com>
2022-02-20 22:48:52 +0530

    Day 119 Code Uplod

---
+++ Day119/AVal.java
@@ -0,0 +1,10 @@
+public class AVal {
+
+    public static void main(String[] args) {
+        char ch = 'a';
+        int ascii = ch;
+        int castAscii = (int) ch;
+        System.out.println("The ASCII value of " + ch + " is: " + ascii);
+        System.out.println("The ASCII value of " + ch + " is: " + castAscii);
+    }
+}
