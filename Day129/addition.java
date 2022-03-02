6b3552903e020bb366fe4a6942e72e79cdb86bd7
Joy Chakraborty <chakrabortyjoy638@gmail.com>
2022-03-02 22:44:18 +0530

    Day 129 code upload

---
+++ Day129/addition.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class addition {
+    public static void main(String[] args) {
+        int num1 = 10, num2 = 20, sum = 0;
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+        sum = num1 + num2;
+        System.out.println("The sum = " + sum);
+    }
+}
