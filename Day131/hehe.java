999bd4284410d418f529f2d7a1b412519ac34463
Joy Chakraborty <chakrabortyjoy638@gmail.com>
2022-03-04 23:20:32 +0530

    Day 131 code upload

---
+++ Day131/hehe.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.Scanner;
+public class hehe {
+    public static void main(String[] args) {
+        int age;
+        System.out.println("Enter your age: ");
+        Scanner s = new Scanner(System.in);
+        age = s.nextInt();
+        if (age > 18) {
+            System.out.print("You are eligible to VOTE");
+        } else
+            System.out.print("You are not eligible to VOTE");
+    }
+}
