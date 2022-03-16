acc1911a8330a3c3aef2cb1e4620b6211fb08059
Joy Chakraborty <chakrabortyjoy638@gmail.com>
2022-03-16 18:05:03 +0530

    Day 143 code upload

---
+++ Day143/main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args) {
+      String first = "Joy";
+      String second = "loves";
+      String third = "linux";
+      System.out.println(first);   
+      System.out.println(second); 
+      System.out.println(third);  
+    }
+  }
